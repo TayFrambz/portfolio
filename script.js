6bd3f1364b14f96f8848b9af3b094f6f2a487e0b
Tayná Trombin <86115551+TayFrambz@users.noreply.github.com>
2022-07-12 11:16:59 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function redirectLinkedin() {
+    window.location.href = "https://www.linkedin.com/in/taynatrombin/";
+};
+
+function redirectGithub() {
+    window.location.href = "https://github.com/TayFrambz";
+};
+
